4817edca5e95b99b755a482fc81abb0e097c9fa1
Leon Nasswetter <leon.nasswetter@gmail.com>
2020-12-14 14:06:00 -0800

    initial commit

--- index.js
+++ index.js
@@ -28,11 +28,18 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+
+      counter 1 is a function that returns another function
+
   2. Which of the two uses a closure? How can you tell?
   
+      counter1 because it is nesting functions.
+
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+
+      
+      
 */
 
 // counter1 code
@@ -63,8 +70,10 @@ NOTE: This will be a callback function for the tasks below
 */
 
 function inning(/*Code Here*/){
-    /*Code Here*/
+    var randomPoint = Math.round(Math.random() * 2);
+    return randomPoint;
 }
+
 
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
@@ -81,7 +90,7 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
+function finalScore(inning){
   /*Code Here*/
 }
 
